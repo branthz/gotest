33273da050017178c9e1e6c0c3ec0185dd06802b
zhanglei <zhanglei@supercxp.com>
2019-07-26 16:08:56 +0800

    add

---
+++ tmpl/main.go
@@ -0,0 +1,83 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"os"
+)
+
+type Friend struct {
+	Fname string
+}
+
+type Person struct {
+	UserName string
+	Emails   []string
+	Friends  []*Friend
+}
+
+func ifelse() {
+	tEmpty := template.New("template test")
+	tEmpty = template.Must(tEmpty.Parse("空 pipeline if demo: {{if ``}} 不会输出. {{end}}\n"))
+	tEmpty.Execute(os.Stdout, nil)
+
+	tWithValue := template.New("template test")
+	tWithValue = template.Must(tWithValue.Parse("不为空的 pipeline if demo: {{if `anything`}} 我有内容，我会输出. {{end}}\n"))
+	tWithValue.Execute(os.Stdout, nil)
+
+	tIfElse := template.New("template test")
+	tIfElse = template.Must(tIfElse.Parse("if-else demo: {{if `anything`}} if部分 {{else}} else部分.{{end}}\n"))
+	tIfElse.Execute(os.Stdout, nil)
+}
+
+func field() {
+	tpl := template.New("test")
+	t, err := tpl.Parse(`{{define "T"}}Hello, {{.}}!{{end}}`)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	t.ExecuteTemplate(os.Stdout,"T", "xinjiang")
+}
+
+func main() {
+	//ifelse()
+	//field()
+	embedding()
+}
+
+func embedding(){
+	s1, err := template.ParseFiles("html/header.tmpl", "html/content.tmpl", "html/footer.tmpl")
+	if err!=nil{
+		fmt.Println(err)
+		return 
+	}
+	s1.ExecuteTemplate(os.Stdout, "header", nil)
+	fmt.Println()
+	s1.ExecuteTemplate(os.Stdout, "content", nil)
+	fmt.Println()
+	s1.ExecuteTemplate(os.Stdout, "footer", nil)
+	fmt.Println()
+	s1.Execute(os.Stdout, nil)
+}
+
+
+func strAndArrow() {
+	f1 := Friend{Fname: "minux.ma"}
+	f2 := Friend{Fname: "xushiwei"}
+	t := template.New("fieldname example")
+	t, _ = t.Parse(`hello {{.UserName}}!
+            {{range .Emails}}
+                an email {{.}}
+            {{end}}
+            {{with .Friends}}
+            {{range .}}
+                my friend name is {{.Fname}}
+            {{end}}
+            {{end}}
+            `)
+	p := Person{UserName: "Astaxie",
+		Emails:  []string{"astaxie@beego.me", "astaxie@gmail.com"},
+		Friends: []*Friend{&f1, &f2}}
+	t.Execute(os.Stdout, p)
+}
